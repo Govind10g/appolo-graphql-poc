972b2d7ddc1d4b119ee76b663da2f3ee3599ac25
Govind Gupta <37750644+Govind10g@users.noreply.github.com>
2020-02-09 13:34:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -42,6 +42,5 @@ app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
 app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
 // Start the server
-app.listen(3000, () => {
-  console.log('Go to http://localhost:3000/graphiql to run queries!');
-});
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
